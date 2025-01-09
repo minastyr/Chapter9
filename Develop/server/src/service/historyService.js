fbd68d43df62049122e3d3e2062e9af6d3b5d343
Joe Vargas <minastyr@gmail.com>
2025-01-08 19:16:34 -0500

    Add files via upload

---
+++ Develop/server/src/service/historyService.js
@@ -0,0 +1,5 @@
+// TODO: Define a City class with name and id properties
+// TODO: Complete the HistoryService class
+class HistoryService {
+}
+export default new HistoryService();
---
+++ Develop/server/src/service/weatherService.js
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+// TODO: Define an interface for the Coordinates object
+// TODO: Define a class for the Weather object
+// TODO: Complete the WeatherService class
+class WeatherService {
+}
+export default new WeatherService();
